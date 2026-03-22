4fd064d5bc292f132ebe381dfbcdac15a10e3aec
Ananya Mishra <2k24.cs1c.2410091@gmail.com>
2026-03-22 21:15:48 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,44 @@
+import { Switch, Route, Router as WouterRouter } from "wouter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { AppLayout } from "./components/layout/AppLayout";
+import Planner from "./pages/Planner";
+import MissionDashboard from "./pages/MissionDashboard";
+import NotFound from "./pages/not-found";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
+function Router() {
+  return (
+    <AppLayout>
+      <Switch>
+        <Route path="/" component={Planner} />
+        <Route path="/mission/:id" component={MissionDashboard} />
+        <Route component={NotFound} />
+      </Switch>
+    </AppLayout>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <WouterRouter base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+          <Router />
+        </WouterRouter>
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
